182bb3ed66a013407b74935679035a6f09137f9b
bijeshsingha <singhabijesh7@gmail.com>
2023-10-26 18:22:56 +0530

    new commit

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,7 +1,7 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
-
+/*
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
@@ -18,3 +18,4 @@ export async function POST() {
     return NextResponse.json({ error });
   }
 }
+*/
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex flex-wrap items-center justify-between mx-auto px-4">
         <Link
-          href={"/"}
+          href="#home"
           className="text-2xl md:text-5xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#087EE1] to to-[#05E8BA]"
         >
           <span>{`{`}</span>
