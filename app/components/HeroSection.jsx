933795bfe83ca88271071fed374d973b07907607
bijeshsingha <singhabijesh7@gmail.com>
2023-11-01 10:18:43 +0530

    new commit 444

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,30 +1,31 @@
 "use client";
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import EmailSection from "./EmailSection";
 import Link from "next/link";
 
 const HeroSection = () => {
-
-
-    const downloadFileAtURL = (url) =>{
-        const aTag = document.createElement('a')
-        aTag.href = url
-        aTag.setAttribute('download','')
-        document.appendChild(aTag)
-        aTag.click()
-        aTag.remove()
-    }
+  const downloadFileAtURL = (url) => {
+    const aTag = document.createElement("a");
+    aTag.href = url;
+    aTag.setAttribute("download", "");
+    document.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
+  };
 
   return (
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-12">
-        <div className="col-span-7 place-self-center text-center sm:text-left m
-        r-0 md:mr-3">
+        <div
+          className="col-span-7 place-self-center text-center sm:text-left m
+        r-0 md:mr-3"
+        >
           <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#087EE1] to to-[#05E8BA]">
-              Hello, I'm<br/>
+              Hello, I'm
+              <br />
             </span>
             <TypeAnimation
               sequence={[
@@ -42,14 +43,19 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base mb-6 sm:text-lg lg:text-xl">
-            Driven by ReactJS expertise. Crafting seamless, user-focused web experiences.
+            Driven by ReactJS expertise. Crafting seamless, user-focused web
+            experiences.
           </p>
           <div className="">
-            <Link href="#contact" className="px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-white hover:bg-slate-200 text-white bg-gradient-to-br from-[#087EE1] to-[#05E8BA]">
-              Hire Me
-            </Link>
+            <button className="px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-white hover:bg-slate-200 text-white bg-gradient-to-br from-[#087EE1] to-[#05E8BA]">
+              <Link href="#contact">Hire Me</Link>
+            </button>
             <button className="px-1 py-1 rounded-full w-full bg-gradient-to-br from-[#087EE1] to to-[#05E8BA] sm:w-fit mr-4 bg-transparent hover:bg-slate-800 text-white  mt-3">
-              <a href="/resume/Bijesh_Singha_.pdf" download className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              <a
+                href="/resume/Bijesh_Singha_.pdf"
+                download
+                className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2"
+              >
                 Download CV
               </a>
             </button>
