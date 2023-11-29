d102db0375ce09fabbb8909337727f63569efa61
bijeshsingha <singhabijesh7@gmail.com>
2023-11-29 17:24:39 +0530

    centering footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+    <footer className="footer border flex justify-center border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
         <div className="font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#087EE1] to to-[#05E8BA]"><span>{`{`}</span>
         <span className="text-white">bijesh.dev</span>
