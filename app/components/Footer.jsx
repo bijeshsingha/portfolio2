dc9ebb2b4b3acbf0b1b4b98f88ead4a302ec0a0d
bijeshsingha <singhabijesh7@gmail.com>
2023-11-29 21:58:24 +0530

    scrolling fix

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+'use client'
+import React,{useEffect} from "react";
 
 const Footer = () => {
+  useEffect(() => {
+    const handleScroll = (event) => {
+      // Check if the user is scrolling down
+      if (event.deltaY > 0) {
+        // Prevent the default behavior (scrolling down)
+        event.preventDefault();
+      }
+    };
+
+    // Add the scroll event listener when the component mounts
+    window.addEventListener('wheel', handleScroll);
+
+    // Remove the scroll event listener when the component unmounts
+    return () => {
+      window.removeEventListener('wheel', handleScroll);
+    };
+  }, []);
   return (
     <footer className="footer border flex justify-center border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
